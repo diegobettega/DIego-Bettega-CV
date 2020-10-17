e8271e849c5eac5404cbfbf5aca728c8403ba2e0
Diego Bettega <diego.bettega@kinesso.com>
2020-10-17 15:32:05 +0100

    Updates

---
+++ precache-manifest.a491e7878f949296c41fa05f12fe78c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38562f63fe4995406b3c8ed07f898388",
+    "url": "/diego-bettega-cv/index.html"
+  },
+  {
+    "revision": "02564b3e1a09e848e46d",
+    "url": "/diego-bettega-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4ce6aefb472207fc653",
+    "url": "/diego-bettega-cv/static/js/2.2aea624e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/diego-bettega-cv/static/js/2.2aea624e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02564b3e1a09e848e46d",
+    "url": "/diego-bettega-cv/static/js/main.91024d5a.chunk.js"
+  },
+  {
+    "revision": "4a2c42ef421c7a8c3dee",
+    "url": "/diego-bettega-cv/static/js/runtime-main.909a9656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/diego-bettega-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
