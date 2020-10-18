cc2023bef253b682730d077e80dbb19186b73cfc
Diego Bettega <diego.bettega@kinesso.com>
2020-10-18 08:56:18 +0100

    Deploy app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,16 @@ import React from "react";
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer class="white-section" id="footer">
-    <div class="container-fluid">
-
-  <br /><br />
-  <p>Diego Bettega Copyright ⓒ {year}</p>
-</div>
-
+    <footer>
+    <div>
+    <a href="mailto:diego.bettega.90@gmail.com" class="link-decoration"><i class="fa fa-envelope footer-symbol"></i></a>
+    &nbsp; &nbsp;
+    <a href="https://www.linkedin.com/in/diego-bettega-59a649b7/" class="link-decoration"><i class="fa fa-linkedin footer-symbol"></i></a>
+    &nbsp; &nbsp;
+    <a href="https://github.com/diegobettega?tab=repositories" class="link-decoration"><i class="fa fa-github footer-symbol"></i></a>
+    <br /><br />
+      <p>ⓒ Copyright {year} Diego Bettega</p>
+      </div>
     </footer>
   );
 }
